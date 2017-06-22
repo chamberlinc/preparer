7c04017b913cfb154b0f8a892d325cf6454aa078
Aaron Berdanier <aaron.berdanier@gmail.com>
2017-06-22 14:03:40 -0400

    testing a push

--- R/helpers.R
+++ R/helpers.R
@@ -9,7 +9,7 @@ na_fill = function(x, tol){
   return(x)
 }
 
-
+# I added something here, just a test
 # stack data files from the same data logger but different dates
 load_stack_file = function(files, gmtoff, logger){
   dates = sub(".*_(.*)_.*\\..*", "\\1", files) # get all dates
